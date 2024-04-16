9a1ba95a184da216370a896c590e0238b86ab9ec
Masalytin <denissmslt@gmail.com>
2024-04-16 21:22:10 +0300

    - Edited translate task answear check - Added login and registration controllers - Added user controller - Added responses in cotrollers

--- src/main/java/ua/dmjdev/service/ChatAssistantService.java
+++ src/main/java/ua/dmjdev/service/ChatAssistantService.java
@@ -2,7 +2,9 @@ package ua.dmjdev.service;
 
 import org.springframework.ai.chat.ChatClient;
 import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.ai.openai.api.OpenAiApi.ChatCompletionMessage.Role;
 import org.springframework.stereotype.Service;
 import ua.dmjdev.models.assistent.ChatAssistant;
@@ -10,6 +12,12 @@ import ua.dmjdev.models.assistent.ChatAssistant;
 @Service
 public class ChatAssistantService {
     private final ChatClient chatClient;
+    public final OpenAiChatOptions CHAT_OPTIONS = new OpenAiChatOptions(){{
+        setModel("gpt-4");
+        setTemperature(0.8f);
+        setMaxTokens(50);
+        setTopP(0.7f);
+    }};
 
     public ChatAssistantService(ChatClient chatClient) {
         this.chatClient = chatClient;
@@ -17,6 +25,6 @@ public class ChatAssistantService {
 
     public Message getResponse(ChatAssistant chat, String userNewMessageContent) {
         chat.addNewMessage(Role.USER, userNewMessageContent);
-        return chatClient.call(new Prompt(chat.getSpringAIMessage())).getResult().getOutput();
+        return chatClient.call(new Prompt(chat.getSpringAIMessage(), CHAT_OPTIONS)).getResult().getOutput();
     }
 }
