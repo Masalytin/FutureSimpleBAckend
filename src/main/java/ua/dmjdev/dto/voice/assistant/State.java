6ecdd418a583fd85e5e1b7078a3c26169a92489f
Masalytin <denissmslt@gmail.com>
2024-04-26 10:23:23 +0300

    Fixed ok input in voice assistant

--- src/main/java/ua/dmjdev/dto/voice/assistant/State.java
+++ src/main/java/ua/dmjdev/dto/voice/assistant/State.java
@@ -5,7 +5,7 @@ public enum State {
     DO_YOU_NEED_VOICE_ASSISTANT("You ask the user if he needs an his assistant to control the application", "YES", "NO"),
     LOGIN_PAGE("", "LOGIN"),
     COURSE("The user chooses which section to go to. The main page is COURSE", "COURSE", "TRAINING", "CHAT_ASSISTANT", "MY_VOCABULARY", "PROFILE"),
-    SELECT_CHAT_ASSISTANT_THEME("The user selects a topic for communication with the chat assistant", "IN_HOSPITAL", "IN_TAXI", "IN_RESTAURANT",
+    SELECT_CHAT_ASSISTANT_THEME("The user selects a  topic for communication with the chat assistant", "IN_HOSPITAL", "IN_TAXI", "IN_RESTAURANT",
             "IN_INTERVIEW", "IN_HOTEL", "FIRS_DATE", "BACK_TO_HOME_PAGE");
 
     String[] answerOptions;
