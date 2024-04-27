2d08c507ee40baebf830b048e3d332bacdeca427
Masalytin <denissmslt@gmail.com>
2024-04-27 12:01:37 +0300

    Some refactoring 4.0

--- src/main/java/ua/dmjdev/dto/voice/assistant/State.java
+++ src/main/java/ua/dmjdev/dto/voice/assistant/State.java
@@ -4,15 +4,11 @@ package ua.dmjdev.dto.voice.assistant;
 public enum State {
     DO_YOU_NEED_VOICE_ASSISTANT("You ask the user if he needs an his assistant to control the application", "YES", "NO"),
     LOGIN_PAGE("The user is on the authorization page", "LOGIN"),
-    COURSE("The user chooses which section to go to. The main page is COURSE", "COURSE", "TRAINING", "CHAT_ASSISTANT", "MY_DICTIONARY", "PROFILE"),
-    SELECT_CHAT_ASSISTANT_THEME("The user selects a  topic for communication with the chat assistant. " +
-            "You can also redirect the user to one of the following endpoints SETTINGS, COURSE, TRAINING, CHAT_ASSISTANT, MY_DICTIONARY, PROFILE. " +
-            "The main page is COURSE.", "IN_HOSPITAL", "IN_TAXI", "IN_RESTAURANT",
-            "IN_INTERVIEW", "IN_HOTEL", "FIRS_DATE"),
-    SETTINGS(" The user is in the settings menu. " +
-            "The user selects a  topic for communication with the chat assistant. " +
-            "You can also redirect the user to one of the following endpoints SETTINGS, COURSE, TRAINING, CHAT_ASSISTANT, MY_DICTIONARY, PROFILE. " +
-            "The main page is COURSE.", "DISABLE_VOICE_ASSISTANT");
+    COURSE("The user chooses which section to go to. The main page is COURSE",
+            "DISABLE_VOICE_ASSISTANT", "COURSE", "TRAINING", "CHAT_ASSISTANT", "MY_DICTIONARY", "PROFILE"),
+    SELECT_CHAT_ASSISTANT_THEME("The user selects a  topic for communication with the chat assistant.",
+            "DISABLE_VOICE_ASSISTANT", "IN_HOSPITAL", "IN_TAXI", "IN_RESTAURANT",
+            "IN_INTERVIEW", "IN_HOTEL", "FIRS_DATE");
 
     String[] answerOptions;
     private String description;
